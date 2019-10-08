eeefe761f066e4b83fbc900394c6f35005a5b2be
FatherofDharma <fatherofdharma@gmail.com>
2019-10-07 19:57:46 -0700

    add fonts and functional async code for loading.

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "rough-typewriter": require("./assets/fonts/rough_typewriter.otf"),
+    "rough-typewriter-itl-bold": require("./assets/fonts/rough_typewriter-bld-itl.otf"),
+    "rough-typewriter-itl": require("./assets/fonts/rough_typewriter-itl.otf")
+  });
+};
+
 export default function App() {
+  const [loadedFont, setLoadedFont] = useState(false);
+
+  if (!loadedFont) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setLoadedFont(true)}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>ScholarLess</Text>
+      <Text style={styles.textTest}>ScholarLess</Text>
     </View>
   );
 }
@@ -15,5 +36,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  textTest: {
+    fontFamily: "rough-typewriter-itl-bold",
+    fontSize: 20
   }
 });
